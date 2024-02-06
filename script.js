b5d68afee8544b0428c1753591aac7c29636e013
Tiago Da Silva <tiagopdasgithub@gmail.com>
2024-02-05 22:17:17 -0500

    js
    iniciando com js, para a conversão das moedas

---
+++ script.js
@@ -0,0 +1,39 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector('.currency-select')
+
+function convertValues(){
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")//valor do real
+    const currencyValueConverted = document.querySelector(".currency-value")//valor das outras moedas
+
+    console.log(currencySelect.value)
+    const dolarToday = 5.2
+    const euroToday = 6.2
+
+   
+
+if(currencySelect.value ==  "dolar"){
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US",{
+        style: "currency",
+        currency: "USD"
+    }).format(inputCurrencyValue / dolarToday)
+}
+if(currencySelect.value == "euro"){
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE",{
+        style: "currency",
+        currency: "EUR"
+    }).format(inputCurrencyValue / euroToday)
+}
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+   
+
+    console.log(convertedValue)
+}
+
+convertButton.addEventListener("click", convertValues)
